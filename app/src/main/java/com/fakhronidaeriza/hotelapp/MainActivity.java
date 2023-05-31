6f92fe1d0014e58a1dc09fffd74a839bc9e5c59e
fakhroni <fakhroni.daeriza12@gmail.com>
2023-05-31 14:06:30 +0700

    HotelApp

---
+++ app/src/main/java/com/fakhronidaeriza/hotelapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fakhronidaeriza.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.ImageDecoder;
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView _imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        String imageUrl = "https://s3.bukalapak.com/uploads/content_attachment/d020552830e8d762c41284c5/w-740/shutterstock_546118867-2.jpg.webp";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+    }
+}
+
